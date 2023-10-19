af23abf9880305d3975ac76cb04e33c56b049406
Sarah Goodell <sarahlgoodell1@gmail.com>
2023-10-19 18:58:06 -0400

    Added foreign keys to my Comment model

--- models/comment.js
+++ models/comment.js
@@ -21,7 +21,23 @@ Comment.init (
         type: sequelize.DATE,
         allowNull: true,
         defaultValue: sequelize.NOW,
-    }
+    },
+    user_id: {
+        //Foreign Key
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'user',
+            key: 'id',
+        },
+    },
+    post_id: {
+        //Foreign Key
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'post',
+            key: 'id',
+        },
+    },
 },
 {
     sequelize,
