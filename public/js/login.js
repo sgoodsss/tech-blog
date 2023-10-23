8f71645c2e2fb2c773da9a2ba61187954b3ee251
Sarah Goodell <sarahlgoodell1@gmail.com>
2023-10-23 16:10:47 -0400

    Added new function for login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,40 +1,27 @@
-// let loginBtn= document.getElementById("loginBtn");
+const loginFormHandler = async (event) => {
+  event.preventDefault();
 
-//Function to handle login logic
-var loginFormHandler = async (event) => {
-    event.preventDefault();
+  // Collect values from the login form
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
 
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
+  if (email && password) {
+    // Send a POST request to the API endpoint
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
 
-    if (email && password) {
-      
-      const response = await fetch('/api/user/login', {
-        method: 'POST',
-        body: JSON.stringify({ email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/profile');
-      } else {
-        loginError()
-      }
+    if (response.ok) {
+      // If successful, redirect the browser to the profile page
+      document.location.replace('/profile');
+    } else {
+      response.status(400).json(err);
     }
-  };
-
-// Function to display error message 
-function loginError() {
-  document.querySelector('#error').innerHTML= 
-  '<div class="alert alert-danger text-center m-3 p-3" role="alert">Failed to login :-( <br> Try a different username or password ! </div>';
-  return('Failed to log in')
+  }
 };
 
- //Event listener v2 - removes console error
-$(document).on('submit', '.login-form', function (event) {
-    loginFormHandler(event)
-  });
-
-// loginBtn.addEventListener('submit', function(event){
-//   loginFormHandler(event)
-// });
+document
+  .querySelector('.login-form')
+  .addEventListener('submit', loginFormHandler);
